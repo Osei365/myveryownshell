d9c60ea45993a06d80d21881dd4e617cefbdd03e
Osei <williamsabodunrin@gmail.com>
2023-05-25 13:15:06 +0000

    simple shell

--- _atoi.c
+++ _atoi.c
@@ -1,39 +1,39 @@
-#include "shell.h"
+#include "main.h"
 
 /**
- * interactive - returns true if shell is interactive mode
- * @info: struct address
+ * shellinteractive - returns true if the shell is in interactive
+ * @arg: argument address
  *
- * Return: 1 if interactive mode, 0 otherwise
+ * Return: 1 else 0
  */
-int interactive(info_t *info)
+int interactive(arg_t *arg)
 {
-	return (isatty(STDIN_FILENO) && info->readfd <= 2);
+	return (isatty(STDIN_FILENO) && arg->readfd <= 2);
 }
 
 /**
- * is_delim - checks if character is a delimeter
- * @c: the char to check
- * @delim: the delimeter string
- * Return: 1 if true, 0 if false
+ * is_delim - confirms delimiter
+ * @ch: char
+ * @delim: delimiter
+ * Return: 1 else 0
  */
-int is_delim(char c, char *delim)
+int is_delim(char ch, char *delim)
 {
-	while (*delim)
-		if (*delim++ == c)
+	for (; *delim; *delim++)
+		if (*delim == ch)
 			return (1);
 	return (0);
 }
 
 /**
- *_isalpha - checks for alphabetic character
- *@c: The character to input
- *Return: 1 if c is alphabetic, 0 otherwise
+ *_isalpha - confirms alpha characters
+ *@ch: char
+ *Return: 1 else 0
  */
 
-int _isalpha(int c)
+int _isalpha(int ch)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
 		return (1);
 	else
 		return (0);
@@ -41,31 +41,31 @@ int _isalpha(int c)
 
 /**
  *_atoi - converts a string to an integer
- *@s: the string to be converted
- *Return: 0 if no numbers in string, converted number otherwise
+ *@str:the string to be converted
+ *Return: 0 if string contains no numbers
  */
 
-int _atoi(char *s)
+int _atoi(char *str)
 {
-	int i, sign = 1, flag = 0, output;
+	int a, sn = 1, flag = 0, output;
 	unsigned int result = 0;
 
-	for (i = 0;  s[i] != '\0' && flag != 2; i++)
+	for (a = 0;  str[a] != '\0' && flag != 2; a++)
 	{
-		if (s[i] == '-')
-			sign *= -1;
+		if (str[a] == '-')
+			sn *= -1;
 
-		if (s[i] >= '0' && s[i] <= '9')
+		if (str[a] >= '0' && str[a] <= '9')
 		{
 			flag = 1;
 			result *= 10;
-			result += (s[i] - '0');
+			result += (str[a] - '0');
 		}
 		else if (flag == 1)
 			flag = 2;
 	}
 
-	if (sign == -1)
+	if (sn == -1)
 		output = -result;
 	else
 		output = result;
