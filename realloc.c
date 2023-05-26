962b6caf447cdd37fa44b6c49e97528cc01a2d17
Osei <williamsabodunrin@gmail.com>
2023-05-26 00:20:38 +0000

    realloc memset

--- realloc.c
+++ realloc.c
@@ -1,4 +1,4 @@
-#include "shell.h"
+#include "main.h"
 
 /**
  **_memset - fills memory with a constant byte
@@ -32,31 +32,43 @@ void ffree(char **pp)
 }
 
 /**
- * _realloc - reallocates a block of memory
- * @ptr: pointer to previous malloc'ated block
- * @old_size: byte size of previous block
- * @new_size: byte size of new block
- *
- * Return: pointer to da ol'block nameen.
+ * _realloc - re allocates memory
+ * @ptr: pointer
+ * @old_size: old size
+ * @new_size: new_size
+ * Return: void pointer
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *p;
+	void *result = NULL;
 
 	if (!ptr)
+	{
 		return (malloc(new_size));
-	if (!new_size)
-		return (free(ptr), NULL);
-	if (new_size == old_size)
-		return (ptr);
+	}
+	if (new_size)
+	{
+		if (new_size == old_size)
+		{
+			return (ptr);
+		}
 
-	p = malloc(new_size);
-	if (!p)
-		return (NULL);
-
-	old_size = old_size < new_size ? old_size : new_size;
-	while (old_size--)
-		p[old_size] = ((char *)ptr)[old_size];
-	free(ptr);
-	return (p);
+		result = malloc(new_size);
+		if (result)
+		{
+			if (old_size > new_size)
+			memcpy(result, ptr, new_size);
+			else if (new_size > old_size)
+			memcpy(result, ptr, old_size);
+			free(ptr);
+		}
+	}
+	else
+	{
+		if (ptr)
+		{
+			free(ptr);
+		}
+	}
+	return (result);
 }
